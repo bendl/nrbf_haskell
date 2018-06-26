119abe2d8fbe7776a4b94fb14d5253b8c457648f
Ben Lancaster <bdl@live.co.uk>
2018-06-26 12:46:45 +0100

    preprocess: Clean up get_next_hour
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- preprocess.hs
+++ preprocess.hs
@@ -7,34 +7,11 @@ import Data.List.Split
 import Debug.Trace
 trace' arg = trace (show arg) arg
 
-normalise_date (d:m:h:[]) = [d/31, m/12, h/24]
-
--- Round f to n decimal places
-dp2 f n = (fromInteger $ round $ f * (10^n)) / (10.0^^n)
-
-parse_line :: String -> [Double]
-parse_line line = do
-    let parts       = splitOn "," line
-        date_time   = splitOn " " (parts !! 1)
-        dates       = splitOn "-" (date_time !! 1)
-        times       = splitOn ":" (date_time !! 2)
-        
-        day     = (read (dates !! 2) :: Double)
-        month   = (read (dates !! 1) :: Double)
-        hour    = (read (times !! 0) :: Double)
-        demand  = (read (parts !! 2) :: Double)
-
-        row = (map (\f -> dp2 f 2) $
-            (normalise_date [day, month, hour])) ++ [demand]
-        in
-            row
-
+-- Test data
 d :: [Int]
 d = [1,1,2,3,1,1,2,2,3]
 d2 :: [[Double]]
 d2 = [[0, 0, 0, 0], [0,0,0,0], [0,0,1,0], [0,0,2,0], [0,0,1,0]]
-
-
 d3 = [[3.0e-2,8.0e-2,0.0,32276.0],
     [3.0e-2,8.0e-2,0.0,32190.0],
     [3.0e-2,8.0e-2,0.0,32313.0],
@@ -66,12 +43,42 @@ d3 = [[3.0e-2,8.0e-2,0.0,32276.0],
     [3.0e-2,8.0e-2,8.0e-2,30824.0],
     [3.0e-2,8.0e-2,8.0e-2,30632.0]]
 
+
+normalise_date (d:m:h:[]) = [d/31, m/12, h/24]
+
+-- Round f to n decimal places
+dp2 f n = (fromInteger $ round $ f * (10^n)) / (10.0^^n)
+
+parse_line :: String -> [Double]
+parse_line line = do
+    let parts       = splitOn "," line
+        date_time   = splitOn " " (parts !! 1)
+        dates       = splitOn "-" (date_time !! 1)
+        times       = splitOn ":" (date_time !! 2)
+        
+        day     = (read (dates !! 2) :: Double)
+        month   = (read (dates !! 1) :: Double)
+        hour    = (read (times !! 0) :: Double)
+        demand  = (read (parts !! 2) :: Double)
+
+        row = (map (\f -> dp2 f 2) $
+            (normalise_date [day, month, hour])) ++ [demand]
+        in
+            row
+
 get_next_hour' :: [Double] -> [[Double]] -> [[Double]]
-get_next_hour' h [] = [h]
+get_next_hour' h [] =[h]
 get_next_hour' h t =
-    case ((h !! 2) == ((head t) !! 2)) of
-        True    -> get_next_hour' (head t) (tail t)
-        False   -> [h] ++ (get_next_hour' (head t) (tail t))
+    case (current_hour == next_hour) of
+        True    -> 
+            -- It's from the same hour, skip it
+            get_next_hour' (head t) (tail t)
+        False   -> 
+            -- It's a new hour, append it to our output
+            [h] ++ (get_next_hour' (head t) (tail t))
+    where 
+        current_hour = (h !! 2)
+        next_hour    = ((head t) !! 2)
 
 get_next_hour :: [[Double]] -> [[Double]]
 get_next_hour row = get_next_hour' (head row) (tail row)
@@ -83,18 +90,18 @@ main = do
     filec <- readFile $ args !! 0
     
     let 
-        file_name   = args !! 0
-        file_lines  = lines filec
-        header      = take 1 file_lines
-        file_data   = drop 1 file_lines
-        test_data   = file_data
-        out_data    = map parse_line test_data
+        file_name       = args !! 0
+        file_lines      = lines filec
+        header          = take 1 file_lines
+        file_data       = drop 1 file_lines
+        test_data       = file_data
+        out_data        = map parse_line test_data
         out_data_hourly = get_next_hour out_data
-        out_data_s  = unlines $ map show (trace' out_data_hourly)
+        out_data_s      = unlines $ map show out_data_hourly
 
         in do
             -- print out first 10 hours of data
             putStrLn $ unlines $ map show $ take 10 out_data_hourly
             
             -- Write normalised training data to file
-            --writeFile (file_name ++ ".out.csv") out_data_s
+            writeFile (file_name ++ ".out.csv") out_data_s
