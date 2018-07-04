ee70da76431ed69015cd8f61737f8e09db3732e7
Ben Lancaster <bdl@live.co.uk>
2018-07-04 23:46:26 +0100

    nrbf: Dynamic add/update hidden nodes depending on training error
    Signed-off-by: Ben Lancaster <bdl@live.co.uk>

--- src/NRBF.hs
+++ src/NRBF.hs
@@ -47,6 +47,18 @@ delta_rule :: DInput -> DInput -> DInput
 delta_rule xold xnew = 
     zipWith (+) (map (* 0.8) xold) (map (* 0.2) xnew)
 
+    --type DOutput  = Double
+    --type DInput   = [Double]
+    --type DWeights = [DInput]
+    --type DRow     = (DInput, DOutput)
+    --type DSet     = [DRow]
+    
+--                [ [] ]
+weight_replace :: DWeights -> Int -> DInput -> DWeights
+weight_replace all_weights idx new_weights = 
+    (take idx all_weights) ++ new_weights : (drop (idx + 1) all_weights)
+
+
 -- Update hidden layer, depending on training result
 --  If 
 update :: DRow -> DWeights -> DWeights
@@ -55,14 +67,23 @@ update td weights = do
     -- 1. Find most active hidden node for (fst DRow)
     -- 2. Apply (0.8 * active hidden node weights) + (0.2 * fst DRow)
     --trace'str "Most active hnode: " most_active_index
-    updated_weights
+    new_weights
 
-    where   td_input = fst td
-            td_expec = snd td
-            most_active_index = maxIndex $ map (\i -> grbf td_input i) weights
-            updated_weights = delta_rule (head weights) td_input
+    where   td_input            = fst td
+            td_expec            = snd td
+            node_activity       = map (\i -> grbf td_input i) weights
+            most_active_index   = maxIndex $ (trace'str "Node activity: " node_activity)
+            most_active_node_hf = node_activity !! most_active_index
+            most_active_node    = weights !! (trace'str "Most active index: " most_active_index)
+            updated_weight      = delta_rule (trace'str "Most active node: " most_active_node) (trace'str "td_input: " td_input)
+            new_weights         = 
+                case (most_active_node_hf >= 0.7) of
+                    True    -> weight_replace weights most_active_index (trace'str "Updated weights: " updated_weight)
+                    False   -> weights ++ (trace'str "Adding weight: " [td_input])
 
 td = [([5.0], 1.5)]
+td2 = [([5.0], 1.5), ([3.0], 0.8), ([2.8], 0.6)]
+
 -- type DSet     = [(DInput, DOutput)]
 -- type DSet     = [([0], 0)]
 -- Training accepts an input data set, an initial set of weights,
@@ -77,13 +98,13 @@ train' dataset weights = do
     trace ("Train Err: " ++ (show train_err)) weights
 
     -- TODO: Change to test RMS error, not train error
-    case (train_err <= 0.1)  of
+    case ((abs train_err) <= 0.1)  of
         True    ->
             -- Good prediction, don't change hidden layer
             weights
         False   ->
             -- Bad prediction, update hidden layer
-            train' (trace'str "\n\nNext dataset: " tdn) (update tdi weights)
+            train' (trace'str "\n\nNext dataset: " tdn) (trace'str "New net nodes: " updated_net)
 
     where
         tdi         = trace'str "tdi: " $ head dataset
@@ -93,3 +114,4 @@ train' dataset weights = do
         nn_out      = net td_input weights
         train_err   = (trace'str "NN_expec: " td_expec) 
                         - (trace'str "NN_output: " nn_out)
+        updated_net = update tdi weights
